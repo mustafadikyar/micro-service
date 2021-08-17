4e12bf6c955dc75750b8592200885d7ebe8503a7
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-17 15:43:31 +0300

    Category endpoints.

---
+++ src/server/services/Micro.Catalog/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Micro.Catalog.DTOs;
+using Micro.Catalog.Services;
+using Micro.Shared.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Micro.Catalog.Controllers
+{
+    [Route("api/categories")]
+    public class CategoryController : BaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService) => _categoryService = categoryService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAllAsync();
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _categoryService.GetByIdAsync(id);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CategoryCreateDTO model)
+        {
+            var response = await _categoryService.CreateAsync(model);
+            return CreateActionResultInstance(response);
+        }
+    }
+}
