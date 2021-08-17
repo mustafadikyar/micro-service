42a012406687b0cdfa295e963f61d0040cbd559f
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-17 15:38:50 +0300

    BaseController configuration.

---
+++ src/server/Micro.Shared/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Micro.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micro.Shared.Controllers
+{
+    [Route("api/courses")]
+    [ApiController]
+    public class BaseController
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response) => 
+            new ObjectResult(response) { StatusCode = response.Status };
+    }
+}
