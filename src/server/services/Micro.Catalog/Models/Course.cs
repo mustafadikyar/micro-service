b17b08e693f11f7c21fee0117a139c5369e25062
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-17 14:51:51 +0300

    MongoDb Entities.

---
+++ src/server/services/Micro.Catalog/Models/Course.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Micro.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+        public string UserId { get; set; }
+
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonIgnore] //Bu property'i göz ardı et.
+        public Category Category { get; set; }
+    }
+}
---
+++ src/server/services/Micro.Catalog/Models/Feature.cs
@@ -0,0 +1,7 @@
+﻿namespace Micro.Catalog.Models
+{
+    public class Feature
+    {
+        public int Duration { get; set; }
+    }
+}
