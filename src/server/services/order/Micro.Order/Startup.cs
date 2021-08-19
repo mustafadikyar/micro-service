a8299b1032249bd293419c9814a199fc204b0d26
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-19 16:26:48 +0300

    Bugfix. & Sql Migration.

--- src/server/services/order/Micro.Order/Startup.cs
+++ src/server/services/order/Micro.Order/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Micro.Order
 {
@@ -26,6 +20,11 @@ namespace Micro.Order
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Micro.Order.Infrastructure.OrderDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), 
+                    configure => configure.MigrationsAssembly("Micro.Order.Infrastructure"));
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
