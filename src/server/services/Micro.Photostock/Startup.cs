8ccc77e4ed6f54d88d2858dbb5a315738c8c417c
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-19 10:56:38 +0300

    Photostock Auth & Test OK.

--- src/server/services/Micro.Photostock/Startup.cs
+++ src/server/services/Micro.Photostock/Startup.cs
@@ -1,16 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Micro.Photostock
 {
@@ -26,8 +21,18 @@ namespace Micro.Photostock
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option =>
+            {
+                option.Authority = Configuration["IdentityServerURL"];
+                option.Audience = "resource_photo_stock"; //Token içerisinde olmak zorunda.
+                option.RequireHttpsMetadata = false; //Ssl zorunluluðunu kaldýrýr.
+            });
 
-            services.AddControllers();
+            services.AddControllers(option =>
+            {
+                option.Filters.Add(new AuthorizeFilter()); //Tüm endpointlere authorize ekler.
+            });
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Micro.Photostock", Version = "v1" });
@@ -47,6 +52,8 @@ namespace Micro.Photostock
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
