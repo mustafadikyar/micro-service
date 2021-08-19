6795acdca63a69ca8077a5f338e9a256d66dcbd8
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-19 16:49:28 +0300

    Fake Payment.

---
+++ src/server/services/payment/Micro.Payment/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Micro.Payment.DTOs;
+using Micro.Shared.Controllers;
+using Micro.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micro.Payment.Controllers
+{
+    [Route("api/payments")]
+    public class PaymentController : BaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment(PaymentDTO paymentDto) => //payment operations
+            CreateActionResultInstance(Response<NoContent>.Success(200));
+    }
+}
