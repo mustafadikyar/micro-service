d032cc23638105dc97550289edab3a90316bac25
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-19 11:01:40 +0300

    Basket DTOs & Common Configurations.

---
+++ src/server/services/Micro.Basket/DTOs/BasketDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Micro.Basket.DTOs
+{
+    public class BasketDTO
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public List<BasketItemDTO> Items { get; set; }
+
+        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+    }
+}
---
+++ src/server/services/Micro.Basket/DTOs/BasketItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Micro.Basket.DTOs
+{
+    public class BasketItemDTO
+    {
+        public string CourceId { get; set; }
+        public string CourceName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
