9d91aa8d0626bb90c831f5d21627e9595176c28f
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-20 09:20:24 +0300

    SingIn View.

---
+++ src/client/web/Micro.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Micro.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
