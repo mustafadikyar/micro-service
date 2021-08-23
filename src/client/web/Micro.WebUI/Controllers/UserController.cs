1d495517bf7103c9b307db3e35d1e4b18fed64ef
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-08-23 12:00:24 +0300

    User Authentication. & Bugfix. Test OK.

--- src/client/web/Micro.WebUI/Controllers/UserController.cs
+++ src/client/web/Micro.WebUI/Controllers/UserController.cs
@@ -1,10 +1,11 @@
 ﻿using Micro.WebUI.Services.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Micro.WebUI.Controllers
 {
-
+    [Authorize]
     public class UserController : Controller
     {
         private readonly IUserService _userService;
